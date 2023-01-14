6428415da7d37790d2b71ab66750c52f2b10540c
jan.yodsawadee <jan.yodsawadee@outlook.com>
2023-01-14 15:27:49 +0700

    simple JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
 
-function App() {
+const App = () => {
+  const name = "JAN";
+  const isNameShowing = true;
+
   return (
     <div className="App">
+      <h1>{2+2}</h1>
+      <h1>Hello, {isNameShowing ? name : 'Someone'}</h1>
+      {name ? (
+        <>{name}</>
+      ) : (
+        <>
+          <h1>test2</h1>
+          <h2>there is no name</h2>
+        </>
+      )}
     </div>
   );
 }
